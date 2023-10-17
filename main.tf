8ac9ac8452f62d2178da842283d91ebc14b3320b
ZheniKoleva <zhenie_koleva@abv.bg>
2023-10-17 10:46:01 +0300

    reversed changes

--- main.tf
+++ main.tf
@@ -5,13 +5,14 @@ terraform {
       version = "3.75.0"
     }
   }
-
+  
   backend "azurerm" {
-    resource_group_name  = var.resource_group_name_storage
-    storage_account_name = var.storage_account_name
-    container_name       = var.storage_container_name
+    resource_group_name  = "StorageRG"
+    storage_account_name = "task-board-storage-zh"
+    container_name       = "task-board-container"
     key                  = "terraform.tfstate"
   }
+
 }
 
 provider "azurerm" {
@@ -21,45 +22,6 @@ provider "azurerm" {
 resource "random_integer" "integer" {
   min = 10000
   max = 99999
-}
-
-resource "azurerm_resource_group" "storage_rg" {
-  name     = var.resource_group_name_storage
-  location = var.resource_group_location
-}
-
-resource "azurerm_storage_account" "storage_account" {
-  name                     = "${var.storage_account_name}${random_integer.integer.result}"
-  resource_group_name      = azurerm_resource_group.storage_rg.name
-  location                 = var.resource_group_location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-  enable_https_traffic_only = true
-
-  depends_on = [azurerm_resource_group.rg]
-}
-
-resource "azurerm_storage_container" "storage_container" {
-  name                  = var.storage_container_name
-  storage_account_name  = azurerm_storage_account.storage_account.name
-  container_access_type = "private"
-}
-
-resource "azurerm_ad_application" "service_principal" {
-  display_name = var.service_principal
-}
-
-resource "azurerm_msi" "service_principal_msi" {
-  name                = var.service_principal_msi_name
-  resource_group_name = azurerm_resource_group.rg.name
-  depends_on = [azurerm_ad_application.service_principal]
-}
-
-resource "azurerm_role_assignment" "service_principal_contributor" {
-  principal_id = azurerm_ad_application.service_principal.application_id
-  role_definition_name = "Contributor"
-  scope = azurerm_resource_group.rg.id
-  depends_on = [azurerm_msi.service_principal_msi]
 }
 
 resource "azurerm_resource_group" "rg" {
